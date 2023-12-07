a7e20dfda1092d609e63b7e0f13eec34648f9beb
Mayur Hanwate <frostblizard111@gmail.com>
2023-12-07 19:27:06 +0530

    commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,8 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
 
 const App = () => {
-  return (
-    <div id="main"></div>
-  )
-}
-
+  return <div id="main">I am learning React. My life is getting better.</div>;
+};
 
 export default App;
